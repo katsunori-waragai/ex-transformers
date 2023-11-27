ee16462149be48ff6e01367db475cc2a98139179
Katsunori Waragau <149886409+katsunori-waragai@users.noreply.github.com>
2023-11-27 17:43:32 +0900

    Feature/senti (#4)
    why
    ハードコードされた入力文での出力しかできない。
    what
    英文を何度でも入力して出力を得ることができる。
    空の入力の場合には終了する。

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -1,6 +1,16 @@
 from transformers import pipeline
 
-# Allocate a pipeline for sentiment-analysis
-classifier = pipeline('sentiment-analysis')
-result = classifier('We are very happy to introduce pipeline to the transformers repository.')
-print(result)
+if __name__ == "__main__":
+    # Allocate a pipeline for sentiment-analysis
+    classifier = pipeline('sentiment-analysis')
+    text = 'We are very happy to introduce pipeline to the transformers repository.'
+    print(text)
+    result = classifier(text)
+    print(result)
+    while True:
+        text = input("English text: ")
+        if len(text) == 0:
+            break
+        print(text)
+        result = classifier(text)
+        print(result)
