e2def2439c299882b6e3588f45d98182d365b061
katsunori-waragai <huyuhiko1128@gmail.com>
2023-11-27 17:18:05 +0900

    you can input another text.

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -1,6 +1,14 @@
 from transformers import pipeline
 
-# Allocate a pipeline for sentiment-analysis
-classifier = pipeline('sentiment-analysis')
-result = classifier('We are very happy to introduce pipeline to the transformers repository.')
-print(result)
+if __name__ == "__main__":
+    # Allocate a pipeline for sentiment-analysis
+    classifier = pipeline('sentiment-analysis')
+    text = 'We are very happy to introduce pipeline to the transformers repository.'
+    print(text)
+    result = classifier(text)
+    print(result)
+    while True:
+        text = input("English text: ")
+        print(text)
+        result = classifier(text)
+        print(result)
