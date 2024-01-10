0efcc0c122817b272ca954ab28a4b8074c0462e1
katsunori-waragai <huyuhiko1128@gmail.com>
2024-01-10 15:09:45 +0900

    add opticalflow.py

---
+++ opticalflow.py
@@ -0,0 +1,12 @@
+import requests
+from PIL import Image
+from transformers import pipeline
+from perceiver.model.vision import optical_flow  # register optical flow pipeline
+
+frame_1 = Image.open(requests.get("https://martin-krasser.com/perceiver/flow/frame_0047.png", stream=True).raw)
+frame_2 = Image.open(requests.get("https://martin-krasser.com/perceiver/flow/frame_0048.png", stream=True).raw)
+
+optical_flow_pipeline = pipeline("optical-flow", model="krasserm/perceiver-io-optical-flow", device="cuda:0")
+rendered_optical_flow = optical_flow_pipeline((frame_1, frame_2), render=True)
+
+Image.fromarray(rendered_optical_flow).save("optical_flow.png")
