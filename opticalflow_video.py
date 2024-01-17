5b904df74c4204f27dc21dae0a8a8953f70432fa
katsunori-waragai <huyuhiko1128@gmail.com>
2024-01-17 14:50:28 +0900

    output concat image video

--- opticalflow_video.py
+++ opticalflow_video.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 from opticalflow2 import (
     compute_grid_indices,
@@ -20,11 +21,13 @@ if __name__ == "__main__":
 
     counter += 1
     out_movie = "opticalflow_out.mp4"
+    out_movie2 = "opticalflow_out2.mp4"
 
     H, W = im1.shape[:2]
 
     fourcc = cv2.VideoWriter_fourcc("m", "p", "4", "v")
     writer = cv2.VideoWriter(str(out_movie), fourcc, 20.0, (W, H))
+    writer2 = cv2.VideoWriter(str(out_movie2), fourcc, 20.0, (2 * W, H))
 
     while True:
         if not r2:
@@ -39,9 +42,12 @@ if __name__ == "__main__":
         used = (t1 - t0) / cv2.getTickFrequency()
         print(f"{counter=} {used=} {im1.shape=}")
         bgr = visualize_flow(flow[0])
+        concat_img = np.hstack([im1, bgr])
         writer.write(bgr)
+        writer2.write(concat_img)
         r1, im1 = cap.read()
         r2, im2 = cap.read()
         counter += 1
 
     writer.release()
+    writer2.release()
